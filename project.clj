980f49689d6cc04aa8948c20a6657387ddec1225
Misha Vakulich <m.vakulich@softteco.com>
2020-08-26 01:57:04 +0300

    up ring

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
                     noir.content.getting-started]}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [lib-noir "0.2.0"]
-                 [compojure "1.1.3"]
+                 [compojure "1.6.2"]
                  [bultitude "0.2.0"]
-                 [ring "1.1.6"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-devel "1.8.1"]
                  [clj-stacktrace "0.2.5"]
                  [org.clojure/tools.macro "0.1.1"]])
