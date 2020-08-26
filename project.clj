0bd2e61a5195cbbe4e8b8dcfe86e382d37be6442
Misha Vakulich <m.vakulich@softteco.com>
2020-08-26 13:02:12 +0300

    prevent old ring

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.touch/noir "1.3.1"
+(defproject org.clojars.touch/noir "1.3.2-SNAPSHOT"
   :description "Noir - a clojure web framework"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -6,7 +6,7 @@
   :codox {:exclude [noir.exception noir.content.defaults
                     noir.content.getting-started]}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lib-noir "0.9.9"]
+                 [lib-noir "0.9.9" :exclusions [ring]]
                  [compojure "1.6.2"]
                  [bultitude "0.2.0"]
                  [ring/ring-core "1.8.1"]
